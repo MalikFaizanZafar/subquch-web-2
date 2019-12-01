3f1574aeb1c54da4a195902853c3868ab197654f
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-12-01 14:16:49 +0500

    navbar setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import {  Routes, RouterModule } from '@angular/router';
+
+const routes : Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
